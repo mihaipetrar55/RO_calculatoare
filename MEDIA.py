5d6b18d6403c7450a8b3adafa070552c595efa92
mihaipetrar55 <133497934+mihaipetrar55@users.noreply.github.com>
2023-05-16 20:59:03 +0300

    Add files via upload

---
+++ MEDIA.py
@@ -0,0 +1,82 @@
+a = int(input("Cu cate numere vrei sa faci media aritmetica?"))
+if a == 2:
+    a2x1 = int(input("Care este primul numar?"))
+    a2x2 = int(input("Care este al doilea numar?"))
+    a2s = (a2x1 + a2x2) / 2
+    print("Media aritmetica este: ",a2s)
+if a == 3:
+    a3x1 = int(input("Care este primul numar?"))
+    a3x2 = int(input("Care este al doilea numar?"))
+    a3x3 = int(input("Care este al treilea numar?"))
+    a3s = (a3x1 + a3x2 + a3x3) / 3
+    print("Media aritmetica este: ", a3s)
+if a == 4:
+    a4x1 = int(input("Care este primul numar?"))
+    a4x2 = int(input("Care este al doilea numar?"))  
+    a4x3 = int(input("Care este al treilea numar?"))
+    a4x4 = int(input("Care este al patrulea numar?"))
+    a4s = (a4x1 + a4x2 + a4x3 + a4x4) / 4
+    print("Media aritmetica este: ", a4s)
+if a == 5:
+    a5x1 = int(input("Care este primul numar?"))
+    a5x2 = int(input("Care este al doilea numar?"))  
+    a5x3 = int(input("Care este al treilea numar?"))
+    a5x4 = int(input("Care este al patrulea numar?"))
+    a5x5 = int(input("Care este al cincilea numar?"))
+    a5s = (a5x1 + a5x2 + a5x3 + a5x4 + a5x5) / 5
+    print("Media aritmetica este: ", a5s)
+if a == 6:
+    a6x1 = int(input("Care este primul numar?"))
+    a6x2 = int(input("Care este al doilea numar?"))  
+    a6x3 = int(input("Care este al treilea numar?"))
+    a6x4 = int(input("Care este al patrulea numar?"))
+    a6x5 = int(input("Care este al cincilea numar?"))
+    a6x6 = int(input("Care este al saselea numar?"))
+    a6s = (a6x1 + a6x2 + a6x3 + a6x4 + a6x5 + a6x6) / 6
+    print("Media aritmetica este: ", a6s)
+if a == 7:
+    a7x1 = int(input("Care este primul numar?"))
+    a7x2 = int(input("Care este al doilea numar?"))
+    a7x3 = int(input("Care este al treilea numar?"))
+    a7x4 = int(input("Care este al patrulea numar?"))
+    a7x5 = int(input("Care este al cincilea numar?"))
+    a7x6 = int(input("Care este al saselea numar?"))
+    a7x7 = int(input("Care este al saptelea numar?"))
+    a7s = (a7x1 + a7x2 + a7x3 + a7x4 + a7x5 + a7x6 + a7x7) / 7
+    print("Media aritmetica este: ", a7s)
+if a == 8:
+    a8x1 = int(input("Care este primul numar?"))
+    a8x2 = int(input("Care este al doilea numar?"))
+    a8x3 = int(input("Care este al treilea numar?"))
+    a8x4 = int(input("Care este al patrulea numar?"))
+    a8x5 = int(input("Care este al cincilea numar?"))
+    a8x6 = int(input("Care este al saselea numar?"))
+    a8x7 = int(input("Care este al saptelea numar?"))
+    a8x8 = int(input("Care este al optulea numar?"))
+    a8s = (a8x1 + a8x2 + a8x3 + a8x4 + a8x5 + a8x6 + a8x7 + a8x8) / 8
+    print("Media aritmetica este: ", a8s)
+if a == 9:
+    a9x1 = int(input("Care este primul numar?"))
+    a9x2 = int(input("Care este al doilea numar?"))
+    a9x3 = int(input("Care este al treilea numar?"))
+    a9x4 = int(input("Care este al patrulea numar?"))
+    a9x5 = int(input("Care este al cincilea numar?"))
+    a9x6 = int(input("Care este al saselea numar?"))
+    a9x7 = int(input("Care este al saptelea numar?"))
+    a9x8 = int(input("Care este al optulea numar?"))
+    a9x9 = int(input("Care este al noualea numar?"))
+    a9s = (a9x1 + a9x2 + a9x3 + a9x4 + a9x5 + a9x6 + a9x7 + a9x8 + a9x9) / 9
+    print("Media aritmetica este: ", a9s)
+if a == 10:
+    a10x1 = int(input("Care este primul numar?"))
+    a10x2 = int(input("Care este al doilea numar?"))
+    a10x3 = int(input("Care este al treilea numar?"))
+    a10x4 = int(input("Care este al patrulea numar?"))
+    a10x5 = int(input("Care este al cincilea numar?"))
+    a10x6 = int(input("Care este al saselea numar?"))
+    a10x7 = int(input("Care este al saptelea numar?"))
+    a10x8 = int(input("Care este al optulea numar?"))
+    a10x9 = int(input("Care este al noualea numar?"))
+    a10x10 = int(input("Care este al zecelea numar?"))
+    a10s = (a10x1 + a10x2 + a10x3 + a10x4 + a10x5 + a10x6 + a10x7 + a10x8 + a10x9 + a10x10) / 10
+    print("Media aritmetica este: ", a10s)
